e36475851c3356533b757f1812b7d4373effe01e
Seanachan <hsinchen.pai1218@gmail.com>
2024-11-01 16:32:49 +0800

    complete hw7-1 (with bonus)

---
+++ NCKU_hw7-1/NCKU_hw7-1/Form2.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace NCKU_hw7_1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.檔案ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.編輯ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.文字ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.結束ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.儲存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.另存新檔ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.剪下ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.複製ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.貼上ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.字型ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.顔色ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.檔案ToolStripMenuItem,
+            this.編輯ToolStripMenuItem,
+            this.文字ToolStripMenuItem,
+            this.結束ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(318, 27);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 檔案ToolStripMenuItem
+            // 
+            this.檔案ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.儲存ToolStripMenuItem,
+            this.另存新檔ToolStripMenuItem});
+            this.檔案ToolStripMenuItem.Name = "檔案ToolStripMenuItem";
+            this.檔案ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.檔案ToolStripMenuItem.Text = "檔案";
+            // 
+            // 編輯ToolStripMenuItem
+            // 
+            this.編輯ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.剪下ToolStripMenuItem,
+            this.複製ToolStripMenuItem,
+            this.貼上ToolStripMenuItem});
+            this.編輯ToolStripMenuItem.Name = "編輯ToolStripMenuItem";
+            this.編輯ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.編輯ToolStripMenuItem.Text = "編輯";
+            // 
+            // 文字ToolStripMenuItem
+            // 
+            this.文字ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.字型ToolStripMenuItem,
+            this.顔色ToolStripMenuItem});
+            this.文字ToolStripMenuItem.Name = "文字ToolStripMenuItem";
+            this.文字ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.文字ToolStripMenuItem.Text = "文字";
+            // 
+            // 結束ToolStripMenuItem
+            // 
+            this.結束ToolStripMenuItem.Name = "結束ToolStripMenuItem";
+            this.結束ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.結束ToolStripMenuItem.Text = "結束";
+            this.結束ToolStripMenuItem.Click += new System.EventHandler(this.結束ToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(800, 419);
+            this.textBox1.TabIndex = 1;
+            // 
+            // 儲存ToolStripMenuItem
+            // 
+            this.儲存ToolStripMenuItem.Name = "儲存ToolStripMenuItem";
+            this.儲存ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.儲存ToolStripMenuItem.Text = "儲存";
+            this.儲存ToolStripMenuItem.Click += new System.EventHandler(this.儲存ToolStripMenuItem_Click);
+            // 
+            // 另存新檔ToolStripMenuItem
+            // 
+            this.另存新檔ToolStripMenuItem.Name = "另存新檔ToolStripMenuItem";
+            this.另存新檔ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.另存新檔ToolStripMenuItem.Text = "另存新檔";
+            this.另存新檔ToolStripMenuItem.Click += new System.EventHandler(this.另存新檔ToolStripMenuItem_Click);
+            // 
+            // 剪下ToolStripMenuItem
+            // 
+            this.剪下ToolStripMenuItem.Name = "剪下ToolStripMenuItem";
+            this.剪下ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.剪下ToolStripMenuItem.Text = "剪下";
+            this.剪下ToolStripMenuItem.Click += new System.EventHandler(this.剪下ToolStripMenuItem_Click);
+            // 
+            // 複製ToolStripMenuItem
+            // 
+            this.複製ToolStripMenuItem.Name = "複製ToolStripMenuItem";
+            this.複製ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.複製ToolStripMenuItem.Text = "複製";
+            this.複製ToolStripMenuItem.Click += new System.EventHandler(this.複製ToolStripMenuItem_Click);
+            // 
+            // 貼上ToolStripMenuItem
+            // 
+            this.貼上ToolStripMenuItem.Name = "貼上ToolStripMenuItem";
+            this.貼上ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.貼上ToolStripMenuItem.Text = "貼上";
+            this.貼上ToolStripMenuItem.Click += new System.EventHandler(this.貼上ToolStripMenuItem_Click);
+            // 
+            // 字型ToolStripMenuItem
+            // 
+            this.字型ToolStripMenuItem.Name = "字型ToolStripMenuItem";
+            this.字型ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.字型ToolStripMenuItem.Text = "字型";
+            this.字型ToolStripMenuItem.Click += new System.EventHandler(this.字型ToolStripMenuItem_Click);
+            // 
+            // 顔色ToolStripMenuItem
+            // 
+            this.顔色ToolStripMenuItem.Name = "顔色ToolStripMenuItem";
+            this.顔色ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.顔色ToolStripMenuItem.Text = "顔色";
+            this.顔色ToolStripMenuItem.Click += new System.EventHandler(this.顔色ToolStripMenuItem_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 247);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 檔案ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 編輯ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 文字ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 結束ToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem 儲存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 另存新檔ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 剪下ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 複製ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 貼上ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 字型ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 顔色ToolStripMenuItem;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
---
+++ NCKU_hw7-1/NCKU_hw7-1/Form2.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+
+namespace NCKU_hw7_1
+{
+    public partial class Form2 : Form
+    {
+        string currentFilePath;
+        public Form2()
+        {
+            InitializeComponent();
+            saveFileDialog1.Filter = "文字檔 (*.txt)|*.txt|自訂文字檔 (*.mytxt)|*.mytxt";
+        }
+
+        private void 結束ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void SaveCustomFile(string filePath)
+        {
+            string fontName = textBox1.Font.Name;
+            float fontSize = textBox1.Font.Size;
+            int colorArgb = textBox1.ForeColor.ToArgb();
+
+            bool isBold = textBox1.Font.Style.HasFlag(FontStyle.Bold);
+            bool isItalic = textBox1.Font.Style.HasFlag(FontStyle.Italic);
+            bool isUnderline = textBox1.Font.Style.HasFlag(FontStyle.Underline);
+
+            string fileHeader = $"[Font={fontName};Size={fontSize};Color={colorArgb};Bold={isBold};Italic={isItalic};Underline={isUnderline}]\n";
+
+            string content = textBox1.Text;
+
+            string fileContent = fileHeader + content;
+
+            File.WriteAllText(filePath, fileContent);
+        }
+        private void SaveFile(string filePath)
+        {
+            if (Path.GetExtension(filePath).ToLower() == ".mytxt")
+            {
+                SaveCustomFile(filePath);
+            }
+            else
+            {
+                File.WriteAllText(filePath, textBox1.Text);
+            }
+        }
+
+        public void LoadFile(string path)
+        {
+            currentFilePath = path;
+            if (Path.GetExtension(path) == ".mytxt")
+            {
+                string[] lines = File.ReadAllLines(path);
+                string[] headerParts = lines[0].Trim('[', ']').Split(';');
+                string fontName = headerParts[0].Split('=')[1];
+                float fontSize = float.Parse(headerParts[1].Split('=')[1]);
+                int colorArgb = int.Parse(headerParts[2].Split('=')[1]);
+
+                bool isBold = bool.Parse(headerParts[3].Split('=')[1]);
+                bool isItalic = bool.Parse(headerParts[4].Split('=')[1]);
+                bool isUnderline = bool.Parse(headerParts[5].Split('=')[1]);
+
+                FontStyle style = FontStyle.Regular;
+                if (isBold) style |= FontStyle.Bold;
+                if (isItalic) style |= FontStyle.Italic;
+                if (isUnderline) style |= FontStyle.Underline;
+
+                textBox1.Font = new Font(fontName, fontSize, style);
+                textBox1.ForeColor = Color.FromArgb(colorArgb);
+
+                textBox1.Text = string.Join("\n", lines, 1, lines.Length - 1);
+            }
+            else
+            {
+                textBox1.Text = File.ReadAllText(path);
+            }
+        }
+
+        private void 剪下ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Cut();
+        }
+
+        private void 複製ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Copy();
+        }
+
+        private void 貼上ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Paste();
+        }
+
+        private void 字型ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                textBox1.Font = fontDialog1.Font;
+            }
+        }
+
+        private void 顔色ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                textBox1.ForeColor = colorDialog1.Color;
+            }
+        }
+
+        private void 另存新檔ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                currentFilePath = saveFileDialog1.FileName;
+                SaveFile(currentFilePath);
+                //MessageBox.Show("檔案複製成功 !.....");
+            }
+        }
+
+        private void 儲存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string selectedFilePath = saveFileDialog1.FileName;
+            if (string.IsNullOrEmpty(currentFilePath))
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    SaveFile(saveFileDialog1.FileName);
+                }
+            }
+            else
+            {
+                SaveFile(currentFilePath);
+            }
+        }
+    }
+}
